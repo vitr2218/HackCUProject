ef3b9f8bbb8c060336c7b42d6f518a2800269e24
Elizabeth Qiu <eqiu21@gmail.com>
2019-02-24 10:40:22 -0700

    sign in sign out functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+
+declare const $;
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
+  email = '';
+  password = '';
+  user;
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  public signIn() {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE).then(() => {
+      return firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        .then((user) => {
+          $('#myModal').modal('toggle');
+          this.user = user.user;
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    });
+  }
+
+  public signOut() {
+    firebase.auth().signOut().then(() => {
+      this.user = null;
+    })
   }
 
 }
